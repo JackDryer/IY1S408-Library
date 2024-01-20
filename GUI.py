dd18280214899c57d7581be61dfcca827d99b0c1
unknown <jackjbld@gmail.com>
2024-01-20 20:44:52 +0000

    made menu a mostly working option menu

--- GUI.py
+++ GUI.py
@@ -4,8 +4,8 @@ colour_scheme = {
     "bg": "#010740",
     "fg":"#ffffff",
     "insertbackground": "#ffffff",
-    "highlightcolor" :"#ff9500",
-    "highlightbackground":"#000000",
+    "highlightcolor" :"#000000",
+    "highlightbackground":"#ff9500",
 }
 highlight_colour_scheme = {
     "bg": "#ff9500",
@@ -18,17 +18,17 @@ class UserInterface:
         self.root = tk.Tk()
         self.root.columnconfigure(0,weight=1)
         self.root.rowconfigure(0,weight=1)
-        self.outputbox = BookList(self.root,self.database.update_description)
+        self.outputbox = BookList(self.root,self.database)
         self.outputbox.set_output(self.database.read())
         Book.database =self.database
         self.root.mainloop()
 
 class BookList:
-    def __init__(self,master,update_func):
+    def __init__(self,master,database):
         self.frame = tk.Frame(master)
         self.frame.grid(sticky="NSEW")
         self.displayed_books= []
-        self.update_func = update_func
+        self.update_func = database
     def set_output(self,data):
         self.data = data
         self.update_output()
@@ -38,25 +38,36 @@ class BookList:
             self.displayed_books.append(Book(self.frame,row,book,self.update_func))
             
 class Book:
-    def __init__(self,master,row,book,update_func):
+    def __init__(self,master,row,book,database):
         self.master = master
         self.row = row
         self.book = book
-        self.update_func = update_func
+        self.database = database
         self.ID = book["book_ID"]
-        for x, i in enumerate(["name","ISBN_num","date","description","quantity","author_name"]):
+        for x, i in enumerate(["name","ISBN_num","date","description","quantity"]):
             self.add_element(x,i)
+        author = tk.StringVar(master)
+        author.set(book["author_name"])
+        authorbox = tk.OptionMenu(master,author,*[i[1] for i in database.show_authors()])
+        new_colour_scheme = colour_scheme.copy()
+        new_colour_scheme.pop("insertbackground")
+        authorbox.config(activebackground=colour_scheme["highlightbackground"],highlightthickness=0,pady=0,**new_colour_scheme)
+        new_colour_scheme.pop("highlightbackground")
+        new_colour_scheme.pop("highlightcolor")
+        print(type(authorbox["menu"]))
+        authorbox["menu"].config(activebackground=colour_scheme["highlightbackground"],activeforeground=colour_scheme["highlightcolor"],borderwidth=0,activeborderwidth = 0,**new_colour_scheme)
+        authorbox.grid(row=row,column=x+1,sticky= "NSEW")
 
     def add_element(self,column,column_name):
         entry = tk.Entry(self.master,**colour_scheme)
-        entry.grid(row=self.row,column=column)
+        entry.grid(row=self.row,column=column,sticky="NSEW")
         entry.insert(tk.END,str(self.book[column_name]))
         entry.column_name = column_name # this is deffinatly not a good idea, should use inheritance, but it doesn't change the funtionality.
         entry.bind("<FocusOut>",lambda x: self.leave_entry(entry))
         entry.bind("<FocusIn>", lambda x: entry.configure(**highlight_colour_scheme))
     def leave_entry(self,entry:tk.Entry):
         entry.configure(**colour_scheme)
-        self.update_func(self.ID,entry.column_name,entry.get())
+        self.database.update_description(self.ID,entry.column_name,entry.get())
 
         
 
