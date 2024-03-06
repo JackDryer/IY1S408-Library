3cd172a2115addfc2a6c50e78a7a1648e58d4138
John Dryer (30091534) <30091534@cyber.comp.southwales.ac.uk>
2024-03-06 16:01:44 +0000

    continued work on filtering

--- GUI.py
+++ GUI.py
@@ -45,19 +45,28 @@ class UserInterface:
         self.database = database.DataBase()
         self.root = tk.Tk()
         self.root.columnconfigure(0,weight=1)
-        self.root.rowconfigure(0,weight=1)
+        self.root.rowconfigure(1,weight=1)
+        self.options = UserOptions(self.root)
         self.outputbox = BookList(self.root,self.database)
         self.outputbox.set_output(self.database.read())
         Book.database =self.database
         self.root.mainloop()
-
+class UserOptions:
+    def __init__(self,root) -> None:
+        id = database.BOOKS_FIELDS[0]
+        self.sorting_strvar = tk.StringVar(value=id)
+        self.sorting_box = tk.OptionMenu(root,self.sorting_strvar,id,Book.author_feild,Book.stock_field,*Book.book_fields)
+        self.sorting_box.grid(row = 0, column= 0, sticky="NSEW")
+    @property
+    def sorting(self) ->str:
+        return self.sorting_strvar.get()
 class BookList:
     def __init__(self,master,database):
         self.frame = tk.Frame(master)
         for i in range(6):
             self.frame.columnconfigure(i,weight=1)
         self.create_headings()
-        self.frame.grid(sticky="NSEW")
+        self.frame.grid(row = 1, column= 0,sticky="NSEW")
         self.displayed_books= []
         self.update_func = database
     def create_headings(self):
